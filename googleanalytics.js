1efe8317f59632dfc6f2f28f67cfc817a17978d2
Alex Mandel <tech@wildintellect.com>
2014-09-07 14:19:34 -0700

    Added google analytics

---
+++ googleanalytics.js
@@ -0,0 +1,15 @@
+/* Compare A Map main js
+Alex Mandel Copyright 2012
+tech@wildintellect.com
+http://blog.wildintellect.com
+
+Live code can be seen on http://compareamap.org
+*/
+
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-18594865-2', 'auto');
+  ga('send', 'pageview');
