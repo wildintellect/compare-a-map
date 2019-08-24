38ef6a3369768e30dfc680b356c328fc5f7c338c
Alex Mandel <tech@wildintellect.com>
2019-08-24 16:38:20 -0700

    experiment with using R to generate the map layout

---
+++ mapview.R
@@ -0,0 +1,11 @@
+# Generate compare a map using mapview
+library(mapview)
+library(leafsync)
+
+center <- c(x=-121.73626,y=38.55515)
+m1 <- mapView(map.types = "Esri.WorldImagery")
+m2 <- mapView(map.types = "OpenStreetMap")
+m3 <- mapView(map.types = "Esri.WorldImagery")
+m4 <- mapView(map.types = "Esri.WorldImagery")
+
+leafsync::sync(m1, m2, m3, m4)
